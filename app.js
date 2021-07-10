dece548842d43042df19c271960406f627d6c540
GVMontibon <feedingtim1234@gmail.com>
2021-07-10 22:51:08 +0800

    Add let port = process.env.PORT || 3000

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
+let port = process.env.PORT || 3000;
 
 
 const phoneBookRoutes = require('./routes/phonebook');
@@ -11,10 +12,13 @@ app.use(express.json());
 app.use('/phonebook' , phoneBookRoutes);
 
 
+
 mongoose.connect(
     process.env.DB_CONNECTION, { useNewUrlParser: true,
          useUnifiedTopology: true }, () =>
          console.log('DB is online')
 );
 
-app.listen(8000);
+app.listen(port, () => {
+    console.log('Connected on localhost');
+});
